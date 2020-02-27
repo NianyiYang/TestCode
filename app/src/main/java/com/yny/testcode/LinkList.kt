7b1e261591f1f2b1e89cd7141fa9d9360e2f6dfd
Nianyi.Yang <nianyi.yang@datayes.com>
2020-02-27 19:15:34 +0800

    合并有序链表

--- app/src/main/java/com/yny/testcode/LinkList.kt
+++ app/src/main/java/com/yny/testcode/LinkList.kt
@@ -20,9 +20,35 @@ class LinkList {
      */
     fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
 
-        // 哨兵节点
-        val perHead = ListNode(-1)
+        var ln1 = l1
+        var ln2 = l2
 
-        return null
+        // 哨兵节点
+        val prevHead = ListNode(-1)
+
+        // prev指针
+        var prev: ListNode = prevHead
+
+        while (ln1 != null && ln2 != null) {
+
+            // 如果 1 小于 2
+            if (ln1.`val` < ln2.`val`) {
+                // prev 下一个结点指向 1
+                prev.next = ln1
+                // 1 前进
+                ln1 = ln1.next
+            } else {
+                prev.next = ln2
+                ln2 = ln2.next
+            }
+
+            // 结束后prev指向自己的下一个结点 前进
+            prev = prev.next!!
+        }
+
+        // 收尾 如果哪一个还不为空 直接prev的next指向即可
+        prev.next = ln1 ?: ln2
+
+        return prevHead.next
     }
 }
