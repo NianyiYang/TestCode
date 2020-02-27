d644bdadcff9c266bc0142857096bcb0b289e0aa
Nianyi.Yang <nianyi.yang@datayes.com>
2020-02-27 18:56:01 +0800

    临时提交

--- app/src/main/java/com/yny/testcode/LinkList.kt
+++ app/src/main/java/com/yny/testcode/LinkList.kt
@@ -19,6 +19,10 @@ class LinkList {
      * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
      */
     fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+
+        // 哨兵节点
+        val perHead = ListNode(-1)
+
         return null
     }
 }
