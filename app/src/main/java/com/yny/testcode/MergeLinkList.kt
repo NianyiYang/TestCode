95fcb8502718198105faebf7150cd575d5c9207c
Nianyi.Yang <269977322@qq.com>
2020-03-04 00:29:26 +0800

    合并有序数组

---
+++ app/src/main/java/com/yny/testcode/MergeLinkList.kt
@@ -0,0 +1,23 @@
+package com.yny.testcode
+
+class MergeLinkList {
+    /**
+     * 三指针倒序遍历，原地操作，空间复杂度 O(1)
+     **/
+    fun merge(A: IntArray, m: Int, B: IntArray, n: Int): Unit {
+        var p = m - 1
+        var q = n - 1
+        var cur = m + n - 1
+
+        // 只需要判断B数组指针位置
+        while (q >= 0) {
+            when {
+                // A数组的指针指向-1时，直接将位置上填入B数组处的值即可
+                p < 0 -> A[cur] = B[q--]
+                A[p] < B[q] -> A[cur] = B[q--]
+                else -> A[cur] = A[p--]
+            }
+            cur--
+        }
+    }
+}
