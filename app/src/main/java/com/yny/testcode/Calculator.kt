c5cd6fc365cdea33a7524b1aeb1fc4e3f6114c06
Nianyi.Yang <269977322@qq.com>
2020-03-15 19:19:48 +0800

    动态规划

--- app/src/main/java/com/yny/testcode/Calculator.kt
+++ app/src/main/java/com/yny/testcode/Calculator.kt
@@ -14,4 +14,22 @@ class Calculator {
     fun add(value1: Int, value2: Int): Int {
         return value1 + value2
     }
+
+    /**
+     * 70. 爬楼梯
+     *
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     */
+    fun climbStairs(n: Int): Int {
+        // 可以用动态规划但是我不知道动态规划怎么写
+        return fibonacci(n, 1, 2)
+    }
+
+    private fun fibonacci(n: Int, first: Int, second: Int): Int {
+        return when (n) {
+            1 -> first
+            else -> fibonacci(n - 1, second, first + second)
+        }
+    }
 }
---
+++ app/src/main/java/com/yny/testcode/DynamicProgramming.kt
@@ -0,0 +1,51 @@
+package com.yny.testcode
+
+import kotlin.math.max
+
+/**
+ * 动态规划相关
+ *
+ * 这里的所有题都用动态规划思想做
+ */
+class DynamicProgramming {
+    /**
+     * 53. 最大子序和
+     *
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     */
+    fun maxSubArray(nums: IntArray): Int {
+        // 最优子结构
+        // 重叠子问题
+        val length = nums.size
+
+        val dp = IntArray(length)
+        dp[0] = nums[0]
+
+        for (i in nums.indices) {
+            if (i > 0) {
+                dp[i] = max(dp[i - 1] + nums[i], nums[i])
+            }
+        }
+
+        return dp.max() ?: 0
+    }
+
+    /**
+     * 70. 爬楼梯
+     */
+    fun climbStairs(n: Int): Int {
+        val dp = IntArray(n + 1)
+
+        for (i in 0..n) {
+            when (i) {
+                0 -> dp[i] = 0
+                1 -> dp[i] = 1
+                2 -> dp[i] = 2
+                else -> dp[i] = dp[i - 1] + dp[i - 2]
+            }
+        }
+
+        return dp[n]
+    }
+
+}
