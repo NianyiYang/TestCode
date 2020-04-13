72f633eb2a8f3ba3419f77ac610999f97abad6a1
Nianyi.Yang <269977322@qq.com>
2020-04-13 22:59:04 +0800

    三数之和

--- app/src/main/java/com/yny/testcode/ExampleNumber.kt
+++ app/src/main/java/com/yny/testcode/ExampleNumber.kt
@@ -106,38 +106,100 @@ class ExampleNumber {
      */
     fun threeSum(nums: IntArray): List<List<Int>> {
 
-        val resultSet = HashSet<List<Int>>()
+//        val resultSet = HashSet<List<Int>>()
+//
+//        // p1 第一个的位置 p2 第一个的值 p3 第二个的位置 p4 第二个的值
+//        val matched = mutableListOf<IntArray>()
+//
+//        // 先正向排序，剪枝
+//        val sortedNums = nums.sorted()
+//
+//        // 如果数组为空，或者排序后第一个就为正数，则不用往下算了
+//        if (sortedNums.isEmpty() || sortedNums[0] > 0) {
+//            return mutableListOf()
+//        }
+//
+//        for (i in sortedNums.indices) {
+//            for (j in i + 1 until sortedNums.size) {
+//                // 如果 i,j 处的数据都大于 0 ，则不用加入数组了
+//                if (sortedNums[i] > 0 && sortedNums[j] > 0) {
+//                    break
+//                } else {
+//                    matched.add(intArrayOf(i, sortedNums[i], j, sortedNums[j]))
+//                }
+//            }
+//        }
+//
+//        for (k in 2 until sortedNums.size) {
+//            for (l in matched) {
+//                if (k != l[0] && k != l[2] && sortedNums[k] + l[1] + l[3] == 0) {
+//                    resultSet.add(listOf(l[1], l[3], sortedNums[k]).sorted())
+//                }
+//            }
+//        }
+//
+//        return resultSet.toList()
 
-        // p1 第一个的位置 p2 第一个的值 p3 第二个的位置 p4 第二个的值
-        val matched = mutableListOf<IntArray>()
-
-        // 先正向排序，剪枝
-        val sortedNums = nums.sorted()
-
-        // 如果数组为空，或者排序后第一个就为正数，则不用往下算了
-        if (sortedNums.isEmpty() || sortedNums[0] > 0) {
+        if (nums.isEmpty() || nums.size < 3) {
             return mutableListOf()
         }
 
-        for (i in sortedNums.indices) {
-            for (j in i + 1 until sortedNums.size) {
-                // 如果 i,j 处的数据都大于 0 ，则不用加入数组了
-                if (sortedNums[i] > 0 && sortedNums[j] > 0) {
-                    break
+        val result = mutableListOf<List<Int>>()
+
+        // 正向排序
+        nums.sort()
+
+        for (i in nums.indices) {
+            // 定义双指针
+            var start = i + 1
+            var end = nums.size - 1
+
+            // 因为是正向排序的
+            // 所以nums[i] > 0 表示后面无论怎么加都 > 0 不需要算
+            if (nums[i] > 0) {
+                break
+            }
+
+            // 这个判断条件要加 去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue
+            }
+
+            while (start < end) {
+
+                if (nums[i] + nums[start] + nums[end] == 0) {
+                    // 符合条件
+                    result.add(
+                        listOf(
+                            nums[i],
+                            nums[start],
+                            nums[end]
+                        )
+                    )
+
+                    // 排除重复的情况
+                    while (start < end && nums[start] == nums[start + 1]) {
+                        start++
+                    }
+
+                    // 同上
+                    while (start < end && nums[end] == nums[end - 1]) {
+                        end--
+                    }
+
+                    start++
+                    end--
+
+                } else if (nums[i] + nums[start] + nums[end] < 0) {
+                    // 和 小于 0 左指针右移
+                    start++
                 } else {
-                    matched.add(intArrayOf(i, sortedNums[i], j, sortedNums[j]))
+                    // 和 大于 0 右指针左移
+                    end--
                 }
             }
         }
 
-        for (k in 2 until sortedNums.size) {
-            for (l in matched) {
-                if (k != l[0] && k != l[2] && sortedNums[k] + l[1] + l[3] == 0) {
-                    resultSet.add(listOf(l[1], l[3], sortedNums[k]).sorted())
-                }
-            }
-        }
-
-        return resultSet.toList()
+        return result
     }
 }
