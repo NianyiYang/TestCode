fe1a0329a77fb61952eee69133beab9bd1c9aede
Nianyi.Yang <nianyi.yang@datayes.com>
2020-04-14 19:46:17 +0800

    临时提交

--- app/src/main/java/com/yny/testcode/ExampleNumber.kt
+++ app/src/main/java/com/yny/testcode/ExampleNumber.kt
@@ -202,4 +202,41 @@ class ExampleNumber {
 
         return result
     }
+
+    /**
+     * 17. 电话号码的字母组合
+     */
+    fun letterCombinations(digits: String): List<String> {
+        val map = hashMapOf(
+            Pair(2, arrayOf('a', 'b', 'c')),
+            Pair(3, arrayOf('d', 'e', 'f')),
+            Pair(4, arrayOf('g', 'h', 'i')),
+            Pair(5, arrayOf('j', 'k', 'l')),
+            Pair(6, arrayOf('m', 'n', 'o')),
+            Pair(7, arrayOf('p', 'q', 'r', 's')),
+            Pair(8, arrayOf('t', 'u', 'v')),
+            Pair(9, arrayOf('w', 'x', 'y', 'z'))
+        )
+
+        val seqs = mutableListOf<Array<Char>>()
+
+        for (digit in digits) {
+            val number = digit.toInt()
+            if(map.contains(number)) {
+                seqs.add(map[number]!!)
+            }
+        }
+
+        calc(seqs)
+
+        return listOf()
+    }
+
+    private fun calc(seqs: MutableList<Array<Char>>) {
+        if (seqs.isNullOrEmpty()) {
+            return
+        }
+
+        while (seqs)
+    }
 }
